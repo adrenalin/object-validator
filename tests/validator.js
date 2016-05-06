6c68286f11dc84d2496cf76d9d38361f5377fd89
Arttu Manninen <arttu@kaktus.cc>
2016-05-06 14:16:50 +0300

    Schema setting in validator

--- tests/validator.js
+++ tests/validator.js
@@ -1,14 +1,60 @@
 import Validator from '../libs/validator';
 import Schema from '../libs/schema';
 
-import { types } from './data';
+import { types, testSchema, testInput } from './data';
 import { isObject } from '../libs/primitives';
 
 import should from 'should';
 import expect from 'expect.js';
 
 describe('Check schema validation', () => {
-  it('Should validate input in the default schema', (done) => {
+  let schema = new Schema(testSchema);
+
+  it('Should accept uninitialized schema in the constructor', (done) => {
+    should(() => {
+      let validator = new Validator(testSchema);
+    })
+    .not.throw(Error);
     done();
   });
+
+  it('Should accept initialized schema in the constructor', (done) => {
+    should(() => {
+      let validator = new Validator(schema);
+
+      if (schema !== validator.schema) {
+        throw new Error('Schema does not match');
+      }
+    })
+    .not.throw(Error);
+    done();
+  });
+
+  it('Should accept uninitialized schema in the setSchema method', (done) => {
+    should(() => {
+      let validator = new Validator();
+      validator.setSchema(testSchema);
+    })
+    .not.throw(Error);
+    done();
+  });
+
+  it('Should accept initialized schema in the setSchema method', (done) => {
+    should(() => {
+      let validator = new Validator();
+      validator.setSchema(schema);
+
+      if (schema !== validator.schema) {
+        throw new Error('Schema does not match');
+      }
+    })
+    .not.throw(Error);
+    done();
+  });
+
+  // it('Should validate input in the default schema', (done) => {
+  //   let validator = new Validator(schema);
+  //   validator.validate(testInput);
+  //   done();
+  // });
 });
