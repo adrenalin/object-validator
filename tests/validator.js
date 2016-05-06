fd0c4c085d1b16a6540316ea0f4218c529908a98
Arttu Manninen <arttu@kaktus.cc>
2016-05-06 11:54:12 +0300

    Validator testing scaffolded

---
+++ tests/validator.js
@@ -0,0 +1,14 @@
+import Validator from '../libs/validator';
+import Schema from '../libs/schema';
+
+import { types } from './data';
+import { isObject } from '../libs/primitives';
+
+import should from 'should';
+import expect from 'expect.js';
+
+describe('Check schema validation', () => {
+  it('Should validate input in the default schema', (done) => {
+    done();
+  });
+});
