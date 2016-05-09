1111a30a922d92ebc787aa1266e4179e7d53d666
Arttu Manninen <arttu@kaktus.cc>
2016-05-09 13:44:15 +0300

    Validation test cases for deep structures

--- tests/schema/validate.js
+++ tests/schema/validate.js
@@ -8,7 +8,7 @@ import should from 'should';
 import expect from 'expect.js';
 
 describe('Check schema validation', () => {
-  it('Should not mind if a non-required field is missing', (done) => {
+  it('should not mind if a non-required field is missing', (done) => {
     let schema = new Schema({
       key: String
     })
@@ -17,7 +17,7 @@ describe('Check schema validation', () => {
     done();
   });
 
-  it('Should mind if a required field is missing', (done) => {
+  it('should mind if a required field is missing', (done) => {
     let schema = new Schema({
       key: {
         type: String,
@@ -32,7 +32,7 @@ describe('Check schema validation', () => {
     done();
   });
 
-  it('Should mind if field type is invalid', (done) => {
+  it('should mind if field type is invalid', (done) => {
     let schema = new Schema({
       key: String
     });
@@ -44,7 +44,7 @@ describe('Check schema validation', () => {
     done();
   });
 
-  it('Should check deep structures', (done) => {
+  it('should check deep structures', (done) => {
     let schema = new Schema({
       p1: {
         p2: {
@@ -52,7 +52,7 @@ describe('Check schema validation', () => {
         }
       }
     });
-    console.log('schema', schema);
+
     expect(schema.validate({
       p1: {
         p2: {
@@ -60,6 +60,101 @@ describe('Check schema validation', () => {
         }
       }
     })).to.be(true);
+
+    done();
+  });
+
+  it('should check deep structures for required', (done) => {
+    let schema = new Schema({
+      p1: {
+        p2: {
+          p3: {
+            type: String,
+            required: true
+          }
+        }
+      }
+    });
+
+    expect(schema.validate({
+      p1: {
+        p2: {
+          p3: 'foo'
+        }
+      }
+    })).to.be(true);
+
+    expect(schema.validate({
+      p1: {
+        p2: {}
+      }
+    })).to.be(false);
+
+    done();
+  });
+
+  it('should accept any type when type is not explicitly defined', (done) => {
+    let schema = new Schema({
+      arr: []
+    });
+
+    expect(schema.validate({
+      arr: [
+        'foo',
+        'bar'
+      ]
+    })).to.be(true);
+
+    let data = [];
+    for (let i in types) {
+      data.push(types[i]);
+    }
+
+    expect(schema.validate({
+      arr: data
+    })).to.be(true);
+
+    done();
+  });
+
+  it('should require array key of correct type', (done) => {
+    let schema = new Schema({
+      arr: [String]
+    });
+
+    expect(schema.validate({
+      arr: [
+        'foo',
+        'bar'
+      ]
+    })).to.be(true);
+
+    expect(schema.validate({
+      arr: [
+        'foo',
+        {
+          deep: false
+        }
+      ]
+    })).to.be(false);
+
+    done();
+  });
+
+  it('should respect array maximum length', (done) => {
+    let schema = new Schema({
+      arr: {
+        type: Array,
+        maxLength: 1
+      }
+    });
+
+    expect(schema.validate({
+      arr: [
+        'foo',
+        'bar'
+      ]
+    })).to.be(false);
 
     done();
   });
